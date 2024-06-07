fe75452a8baa95c8d50cfa0a7b5fd8e1f30584f3
jschwabe <mail@jschwabe.site>
2024-06-07 16:13:19 +0200

    ex02: formatting, scope in header

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -7,51 +7,51 @@ template <typename T>
 class Array {
 
 public:
-	T*          arr;
-	std::size_t _size;
-
 	Array()
-		: arr(new T), _size(0) {}
+		: _arr(new T), _size(0) {}
 	Array(unsigned int n)
 		: _size(n) {
-		arr = new T[_size];
+		_arr = new T[_size];
 	}
 	Array(Array const& src)
-		: arr(new T[src._size]), _size(src._size) {
+		: _arr(new T[src._size]), _size(src._size) {
 		for (std::size_t i = 0; i < src._size; i++) {
-			arr[i] = src.arr[i];
+			_arr[i] = src._arr[i];
 		}
 	}
-	~Array() { delete[] arr; };
+	~Array() { delete[] _arr; };
 
 	Array& operator=(Array const& rhs) {
 		// deep copy with empty check
 		if (this != &rhs) {
-			delete[] arr;
+			delete[] _arr;
 			if (rhs._size > 0) {
-				arr = new T[rhs._size];
+				_arr = new T[rhs._size];
 			}
 			for (std::size_t i = 0; i < rhs._size; i++) {
-				arr[i] = rhs.arr[i];
+				_arr[i] = rhs._arr[i];
 			}
-			this->_size = rhs._size;
+			_size = rhs._size;
 		}
 		return *this;
 	}
+
 	T& operator[](std::size_t idx) {
 		if (idx >= _size) {
 			throw OutOfBoundsException();
 		}
-		return arr[idx];
+		return _arr[idx];
 	}
 
+	std::size_t size() { return _size; }
+
+private:
 	class OutOfBoundsException : public std::exception {
 		const char* what() const throw() {
 			return "Index Out of bounds\n";
 		}
 	};
 
-	std::size_t size() { return _size; }
-
-private:
+	T*          _arr;
+	std::size_t _size;
 };
