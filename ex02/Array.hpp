b585ca710c6b3e5521dfb3829b5129746333013e
jschwabe <mail@jschwabe.site>
2024-06-07 14:15:52 +0200

    ex02: implement basic structure

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -2,19 +2,45 @@
 #include <iostream>
 #include <string>
 
-template <typename T> class Array {
+template <typename T>
+
+class Array {
 
 public:
-	Array();
-	Array(unsigned int n);
-	Array(Array const& src);
-	~Array();
+	T*          arr;
+	std::size_t _size;
 
-	Array& operator=(Array const& rhs);
-	Array& operator[](Array const& rhs);
-	T      type;
+	Array()
+		: arr(new T), _size(0) {}
+	Array(unsigned int n)
+		: _size(n) {
+		arr = new T[_size];
+	}
+	Array(Array const& src)
+		: arr(src.arr), _size(src._size) {
+		*this = src;
+	}
+	~Array() { delete[] arr; };
 
-	std::size_t size();
+	Array& operator=(Array const& rhs) {
+		this->arr   = rhs.arr;
+		this->_size = rhs._size;
+		return *this;
+	}
+	T& operator[](std::size_t idx) {
+		if (idx > _size) {
+			throw OutOfBoundsException();
+		}
+		return arr[idx];
+	}
+
+	class OutOfBoundsException : public std::exception {
+		const char* what() const throw() {
+			return "Index Out of bounds\n";
+		}
+	};
+
+	std::size_t size() { return _size; }
 
 private:
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,9 +1,29 @@
 #include "Array.hpp"
+#include <cstdlib>
 #include <iostream>
 
 #define ARR_INT_LENGTH 5
 #define ARR_STR_LENGTH 10
 
 int main(void) {
-	
+	Array<int> arr_int(ARR_INT_LENGTH);
+
+	std::cout << "Array of int: " << arr_int.size() << std::endl;
+
+	for (unsigned int i = 0; i < ARR_INT_LENGTH; i++) {
+		arr_int[i] = std::rand();
+		std::cout << arr_int[i] << std::endl;
+	}
+
+	Array<std::string> arr_str(ARR_STR_LENGTH);
+
+	arr_str[0] = "Hello";
+	arr_str[1] = "World";
+	arr_str[2] = "!";
+
+	std::cout << "Array of string: " << arr_str.size()
+			  << std::endl;
+	for (unsigned int i = 0; i < ARR_STR_LENGTH; i++) {
+		std::cout << arr_str[i] << std::endl;
+	}
 }
