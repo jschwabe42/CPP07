e5f591530d3698d40d58bbc3f429540d88db9e85
jschwabe <mail@jschwabe.site>
2024-06-07 16:13:01 +0200

    ex02: implement test from subject, deep copy & fix freeing

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -17,18 +17,29 @@ public:
 		arr = new T[_size];
 	}
 	Array(Array const& src)
-		: arr(src.arr), _size(src._size) {
-		*this = src;
+		: arr(new T[src._size]), _size(src._size) {
+		for (std::size_t i = 0; i < src._size; i++) {
+			arr[i] = src.arr[i];
+		}
 	}
 	~Array() { delete[] arr; };
 
 	Array& operator=(Array const& rhs) {
-		this->arr   = rhs.arr;
-		this->_size = rhs._size;
+		// deep copy with empty check
+		if (this != &rhs) {
+			delete[] arr;
+			if (rhs._size > 0) {
+				arr = new T[rhs._size];
+			}
+			for (std::size_t i = 0; i < rhs._size; i++) {
+				arr[i] = rhs.arr[i];
+			}
+			this->_size = rhs._size;
+		}
 		return *this;
 	}
 	T& operator[](std::size_t idx) {
-		if (idx > _size) {
+		if (idx >= _size) {
 			throw OutOfBoundsException();
 		}
 		return arr[idx];
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,28 +2,44 @@
 #include <cstdlib>
 #include <iostream>
 
-#define ARR_INT_LENGTH 5
-#define ARR_STR_LENGTH 10
+#define MAX_VAL 750
 
 int main(void) {
-	Array<int> arr_int(ARR_INT_LENGTH);
-
-	std::cout << "Array of int: " << arr_int.size() << std::endl;
-
-	for (unsigned int i = 0; i < ARR_INT_LENGTH; i++) {
-		arr_int[i] = std::rand();
-		std::cout << arr_int[i] << std::endl;
+	Array<int> numbers(MAX_VAL);
+	int*       mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++) {
+		const int value = rand();
+		numbers[i]      = value;
+		mirror[i]       = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
 	}
 
-	Array<std::string> arr_str(ARR_STR_LENGTH);
-
-	arr_str[0] = "Hello";
-	arr_str[1] = "World";
-	arr_str[2] = "!";
-
-	std::cout << "Array of string: " << arr_str.size()
-			  << std::endl;
-	for (unsigned int i = 0; i < ARR_STR_LENGTH; i++) {
-		std::cout << arr_str[i] << std::endl;
+	for (int i = 0; i < MAX_VAL; i++) {
+		if (mirror[i] != numbers[i]) {
+			std::cerr << "didn't save the same value!!"
+					  << std::endl;
+			return 1;
+		}
 	}
+	try {
+		numbers[-2] = 0;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	try {
+		numbers[MAX_VAL] = 0;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+
+	for (int i = 0; i < MAX_VAL; i++) {
+		numbers[i] = rand();
+	}
+	delete[] mirror; //
+	return 0;
 }
